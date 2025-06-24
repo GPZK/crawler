b596b0c787bf4e94660542bfcfd8364e7a5edcc9
Nikita <roman.komikita@gmail.com>
2025-06-24 17:27:55 +0300

    refactor

--- crawler.py
+++ crawler.py
@@ -7,25 +7,6 @@ url = 'https://books.toscrape.com/'
 result = []
 broken_list = []
 
-async def main(page_url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(page_url) as response:
-            html = await response.text()
-            soup = bs4.BeautifulSoup(html, 'html.parser')
-            depth = 1
-            tasks = []
-            for el in soup.find_all('a'):
-                link = urljoin(page_url, el.get('href'))
-                task = check_page(link, depth + 1)
-                tasks.append(task)
-                all_url = list(map(lambda x: x[0], result))
-                if link not in all_url:
-                    result.append((link, depth))
-            await asyncio.gather(*tasks)
-            output()
-
-                
-
 async def check_page(page_url, depth):
     async with aiohttp.ClientSession() as session:
         async with session.get(page_url) as response:
@@ -34,20 +15,25 @@ async def check_page(page_url, depth):
                 broken_list.append(page_url)
                 return
             soup = bs4.BeautifulSoup(html, 'html.parser')
-            tasks1 = []
+            tasks = []
             for el in soup.find_all('a'):
                 link = urljoin(page_url, el.get('href'))
                 all_url = list(map(lambda x: x[0], result))
                 if link not in all_url:
                     result.append((link, depth))
                     task = check_page(link, depth + 1)
-                    tasks1.append(task)
-            await asyncio.gather(*tasks1)
-            print('checked url', page_url, 'at depth', depth, 'q:', len(tasks1))
+                    tasks.append(task)
+            await asyncio.gather(*tasks)
+            print('checked url', page_url, 'at depth', depth, 'q:', len(tasks))
 
 def output():
     with open('result.txt', 'w') as f:
         f.write(f'{result} \nBroken:\n {broken_list}')
+
+
+async def main(page_url):
+    await check_page(page_url, 0)
+    output()
 
 if __name__ == '__main__':
     asyncio.run(main(url))
